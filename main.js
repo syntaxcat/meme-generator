f4f44c230933a4609f26e3f8ca3b7ff8e9980599
Michaela Bereza <michaelabereza@icloud.com>
2022-02-19 10:05:15 +0200

    remove saved memes tab

--- main.js
+++ main.js
@@ -7,21 +7,12 @@ function onInit() {
 
 function openGallery() {
 	document.querySelector('.editor').style.display = 'none';
-	document.querySelector('.saved-memes').style.display = 'none';
 	document.querySelector('.about').style.display = 'none';
 	document.querySelector('.gallery').style.display = 'block';
-}
-
-function openSavedMemes() {
-	document.querySelector('.editor').style.display = 'none';
-	document.querySelector('.gallery').style.display = 'none';
-	document.querySelector('.about').style.display = 'none';
-	document.querySelector('.saved-memes').style.display = 'block';
 }
 
 function openAboutPage() {
 	document.querySelector('.editor').style.display = 'none';
 	document.querySelector('.gallery').style.display = 'none';
-	document.querySelector('.saved-memes').style.display = 'none';
 	document.querySelector('.about').style.display = 'flex';
 }
